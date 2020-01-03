5b4bfba004402f0824ce332f5ed7e8630e4be9df
Gary Yu <gary.yu@gotts.tech>
2020-01-03 08:28:08 +0800

    Add git hook for rust formatter (#3)

--- api/src/rest.rs
+++ api/src/rest.rs
@@ -1,4 +1,5 @@
-// Copyright 2019 The Gotts Developers
+// Copyright 2018 The Grin Developers
+// Modifications Copyright 2019 The Gotts Developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
